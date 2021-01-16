29be19ab247998bfa108724a95f406b1703f1e1f
hyy000 <1539946916@qq.com>
2021-01-16 09:13:20 +0800

    UART-PC作业

---
+++ UAPT-SPI/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'UAPT-SPI' 
+ * Target:  'UAPT-SPI' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
